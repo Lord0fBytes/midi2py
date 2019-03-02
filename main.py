b5590c808fbbee2386220482b74561be38b425be
Justin Masse <jmasse@webtron.me>
2019-03-02 00:18:43 -0500

    Uploading main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#import tkinter as tk
+
+# if you are still working under a Python 2 version,
+# comment out the previous line and uncomment the following line
+import Tkinter as tk
+
+root = tk.Tk()
+
+w = tk.Label(root, text="Hello Tkinter!")
+w.pack()
+
+root.mainloop()
+
+TODO:
+~ Find out how to parse XML data
+~ Create the grid with naming
